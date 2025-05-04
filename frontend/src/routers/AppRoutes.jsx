ea40fb329a0eed12b9000083c4ed5e36de0bf27d
Abdurazakm <abdulkem5472@gmail.com>
2025-05-04 23:44:39 +0300

    routing for signup login and forgate

--- frontend/src/routers/AppRoutes.jsx
+++ frontend/src/routers/AppRoutes.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
+// Add these:
+import LoginPage from '@/pages/LoginPage';
+import SignupPage from '@/pages/SignupPage';
+import ForgotPassword from '@/pages/ForgotPassword';
+
+// Your other pages:
+import Resources from '@/pages/Resources';
 // import MainLayout from '@/layouts/MainLayout';
 // import Home from '@/pages/Home';
 // import SubmitCase from '@/pages/SubmitCase';
 // import AlertsMap from '@/pages/AlertsMap';
 // import Search from '@/pages/Search';
-import Resources from '@/pages/Resources';
 // import Volunteer from '@/pages/Volunteer';
 // import About from '@/pages/About';
 // import Privacy from '@/pages/Privacy';
@@ -18,23 +24,28 @@ function AppRoutes() {
   return (
     <Router>
       <Routes>
-        {/* <Route element={<MainLayout />}> */}
-          {/* <Route index element={<Home />} />
-          <Route path="submit" element={<SubmitCase />} />
-          <Route path="alerts" element={<AlertsMap />} />
-          <Route path="search" element={<Search />} /> */}
-         <Route path="/" element={<Resources />} />
+        {/* Public Auth Pages */}
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
 
-          {/* <Route path="volunteer" element={<Volunteer />} />
-          <Route path="about" element={<About />} />
-          <Route path="privacy" element={<Privacy />} />
-          <Route path="stories" element={<SuccessStories />} />
-          <Route path="toolkit" element={<OfflineToolkit />} />
-          <Route path="partners" element={<PartnerPortal />} /> */}
-        {/* </Route> */}
+        {/* Main Pages */}
+        <Route path="/resources" element={<Resources />} />
+        {/* <Route path="submit" element={<SubmitCase />} />
+        <Route path="alerts" element={<AlertsMap />} />
+        <Route path="search" element={<Search />} />
+        <Route path="volunteer" element={<Volunteer />} />
+        <Route path="about" element={<About />} />
+        <Route path="privacy" element={<Privacy />} />
+        <Route path="stories" element={<SuccessStories />} />
+        <Route path="toolkit" element={<OfflineToolkit />} />
+        <Route path="partners" element={<PartnerPortal />} /> */}
       </Routes>
     </Router>
   );
 }
 
 export default AppRoutes;
+
+
+
