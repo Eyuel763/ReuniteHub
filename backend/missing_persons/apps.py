00baae27de0753224022514d27e7b6c7f96ba16f
Eyuel763 <eyuelg1997@gmail.com>
2025-05-04 08:22:04 +0400

    add success stories automatically when status of the missing person is 'found'

--- backend/missing_persons/apps.py
+++ backend/missing_persons/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class MissingPersonsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'missing_persons'
+
+    def ready(self):
+        import missing_persons.signals
