f0a23bfb0018f844e4f759af9ea9a004c93aa494
Etsubdink Gashaw <etsubgashaw3@gmail.com>
2025-05-04 16:51:59 +0300

    Update Volunteer.jsx

--- frontend/src/pages/Volunteer.jsx
+++ frontend/src/pages/Volunteer.jsx
@@ -1,10 +1,42 @@
 import UserIcon from "./assets/user_group.png";
 import Check from "./assets/checkmark.png";
+import file from "./assets/Volunteer Handbook Template.pdf";
+import { Route, Routes, Link, useParams, useNavigate } from "react-router-dom";
+import RegVolunteer from "./components/RegVolunteer";
+import VolunteerActivities from "./components/VolunteerActivities";
+
+
+
+
+
+
 
 function Volunteer() {
+
+  /*
+<Routes>
+  <Route path="/reg-volunteer" element={<RegVolunteer />} /> 
+</Routes>;
+*/
+  
+  
+ /* const navigate = useNavigate();
+  
+  };*/
+  
+
+  const handleDownload = () => {
+    const link = document.createElement("a");
+    link.href = file; 
+    link.download = "Volunteer Handbook Template.pdf";
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  };
+
     return (
       <div className=" overflow-hidden  w-screen bg-white m-0 p-0 ">
-        <div className="w-screen h-[500px] bg-gray-500 flex flex-col items-center justify-center">
+        <div className="w-screen h-[400px] bg-gray-500 flex flex-col items-center justify-center">
           <h1>Become a lifeline</h1>
           <p>Help reunite families in Ethiopia</p>
         </div>
@@ -12,25 +44,37 @@ function Volunteer() {
           How You Can Help
         </p>
 
-        <div className="flex flex-row justify-around text-black p-4 ">
-          <div className="flex flex-col items-center justify-center m-4">
-            <div className="w-16 h-16 bg-blue-500 flex items-center justify-center rounded-full"></div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 m-8 p-2 ">
+          <div className="flex flex-col items-center  m-4">
+            <img
+              src={UserIcon}
+              alt="User Icon"
+              className="w-10 h-10 sm:w-20 sm:h-20"
+            />
             <p className="font-bold text-lg">Search Teams</p>
             <p className="text-gray-500">
               Join organized searches in regions across Ethiopia to help find
               missing persons.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center m-4 ">
-            <img src={UserIcon} alt="User Icon" className="w-20 h-20" />
+          <div className="flex flex-col items-center  m-4 ">
+            <img
+              src={UserIcon}
+              alt="User Icon"
+              className="w-10 h-10 sm:w-20 sm:h-20"
+            />
             <p className="font-bold text-lg">Flyer Distribution</p>
             <p className="text-gray-500">
               Print and share missing persons posters in markets, schools, and
               kebeles.
             </p>
           </div>
-          <div className="flex flex-col items-center justify-center m-4">
-            <img src={UserIcon} alt="User Icon" className="w-20 h-20" />
+          <div className="flex flex-col items-center  m-4">
+            <img
+              src={UserIcon}
+              alt="User Icon"
+              className="w-10 h-10 sm:w-20 sm:h-20"
+            />
             <p className="font-bold text-lg">Social Media</p>
             <p className="text-gray-500">
               Amplifiy cases using hashtags like #FindThemEthiopia on social
@@ -47,28 +91,44 @@ function Volunteer() {
               Your time and skills can help reunite families. Sign up to recieve
               notifications about volunteer opportunities.
             </p>
-            <button className="bg-blue-900 text-white w-full   ">
+            <button
+              onClick={() => navigate("/reg-volunteer")}
+              className="bg-blue-900 text-white w-full   "
+            >
               Register as Volunteer
             </button>
           </div>
           <div className="bg-yellow-400 w-full h-auto m-2 rounded-lg ">
-            <div className="bg-yellow-50 w-full h-auto flex flex-col items-start overflow-hidden p-2 ml-1 rounded-lg">
+            <div className="bg-yellow-50 w-full h-auto flex flex-col items-start overflow-hidden p-6 ml-1 rounded-lg space-y-2">
               <p className="text-black text-left text-2xl m-2 font-bold">
                 Training & Saftey
               </p>
               <p className="text-gray-500">
-                <img src={Check} alt="Check" className="w-6 h-6  inline" />Never search alone. Always coordinate with local authorities.
+                <img src={Check} alt="Check" className="w-6 h-6  inline" />
+                Never search alone. Always coordinate with local authorities.
               </p>
               <p className="text-gray-500">
-                <img src={Check} alt="Check" className="w-6 h-6  inline" />Avoid high risk zones without proper guidance.
+                <img src={Check} alt="Check" className="w-6 h-6  inline" />
+                Avoid high risk zones without proper guidance.
               </p>
               <p className="text-gray-500">
-                <img src={Check} alt="Check" className="w-6 h-6  inline" />Respect local customs and engage community leaders.
+                <img src={Check} alt="Check" className="w-6 h-6  inline" />
+                Respect local customs and engage community leaders.
               </p>
               <p className="text-gray-500">
-                <img src={Check} alt="Check" className="w-6 h-6  inline" />Maintain confidentiality of sensitive case information.
+                <img src={Check} alt="Check" className="w-6 h-6  inline" />
+                Maintain confidentiality of sensitive case information.
               </p>
+              <button
+                onClick={handleDownload}
+                className="bg-transparent text-blue-900 w-full border-2 border-blue-900 hover:bg-blue-900 hover:text-white rounded-lg p-2 "
+              >
+                Download Volunteer Handbook
+              </button>
             </div>
+          </div>
+          <div>
+            <VolunteerActivities />
           </div>
         </div>
       </div>
@@ -76,3 +136,4 @@ function Volunteer() {
 }
 
 export default Volunteer;
+
