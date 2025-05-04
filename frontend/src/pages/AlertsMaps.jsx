c882626d78baa12403ff9790be7202d6624a68a3
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-05-04 19:50:58 +0300

    place holder

---
+++ frontend/src/pages/AlertsMaps.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AlertsMaps = () => {
+    return (
+        <div>
+            <h1>Alerts Maps</h1>
+            <p>This is the AlertsMaps page.</p>
+        </div>
+    );
+};
+
+export default AlertsMaps;
