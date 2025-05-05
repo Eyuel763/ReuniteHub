a4828c9222eead7914ea0c29e418904c8fc94c64
Abenezer daniel <abenezerdaniel06@gmail.com>
2025-05-05 12:15:00 +0300

    added some styles

--- frontend/src/pages/Error_page.jsx
+++ frontend/src/pages/Error_page.jsx
@@ -8,21 +8,18 @@ const ErrorPage = () => {
       className="bg-center h-screen flex flex-col relative bg-no-repeat bg-cover sm:bg-contain"
       style={{
         backgroundImage: `url(${gifBackground})`,
-      }}
-    >
-      {/* Error Message */}
-      <span className="text-center text-6xl">
-        <h1 className="text-4xl sm:text-3xl text-black font-bold text-center mt-20">404</h1>
+      }}>
+        
+      <span className="text-center font-serif text-6xl">
+        <h1 className="text-4xl sm:text-6xl text-black font-bold text-center mt-20">404</h1>
         <p className="text-2xl sm:text-xl text-black font-semibold text-center mt-4">Page Not Found</p>
       </span>
 
-      {/* Link to Home */}
       <Link
-        to="/"
-        aria-label="Go back to the homepage"
-        className="absolute bottom-11 left-1/2 transform -translate-x-1/2 text-blue-50 sm:text-xl bg-gray-700 font-bold hover:bg-gray-500 transition duration-300 hover:cursor-pointer rounded-lg px-4 py-2"
-      >
-        Go Back to Home
+          to="/"
+          aria-label="Go back to the homepage"
+          className="absolute bottom-11 left-1/2 transform -translate-x-1/2 text-black-50 sm:text-xl bg-red-400 font-bold hover:bg-blue-400 transition duration-300 hover:cursor-pointer hover:scale-110 rounded-lg px-4 py-2 animate-bounce">
+          Return to Home
       </Link>
     </div>
   );
