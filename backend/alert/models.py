3fa536fd3aa38b9cfcbc4ea55de2ca73a5eb2024
Hermii1 <hermelaejigu4@gmail.com>
2025-05-05 14:47:24 +0300

    removed backend folder

--- backend/alert/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from django.conf import settings
-
-class Alert(models.Model):
-    missing_person = models.ForeignKey('missing_persons.MissingPerson', on_delete=models.CASCADE)
-    message = models.TextField()
-    location = models.CharField(max_length=255)
-    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
